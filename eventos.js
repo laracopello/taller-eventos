1ca96182f28878e07019f651cbfd23ab711e0352
laracopello <laracop2005@gmail.com>
2024-11-27 10:09:55 -0300

    Agregado div con evento y estilos

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Manejador de eventos para el div
+    const div = document.getElementById("boton-div");
+    div.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
